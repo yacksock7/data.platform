1b71c83424049d8a0f45a74f933dd2b4f199bd68
youngrackchoi <yacksock3@naver.com>
2025-02-04 20:43:03 +0900

    데이터 베이스 연동 및 Mybatis dependency 추가

--- src/main/java/choi/oop/configuration/ApplicationConfiguration.java
+++ src/main/java/choi/oop/configuration/ApplicationConfiguration.java
@@ -1,8 +1,15 @@
 package choi.oop.configuration;
 
+import choi.oop.util.SystemEnvUtil;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ApplicationConfiguration {
+
+    @Bean
+    public SystemEnvUtil systemEnvUtil() {
+        return new SystemEnvUtil();
+    }
 
 }
