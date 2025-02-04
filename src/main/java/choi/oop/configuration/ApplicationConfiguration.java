6de426430d88ba65cd592bbea1dcd2eda9f1eebe
youngrackchoi <yacksock3@naver.com>
2025-02-05 00:31:35 +0900

    ModelMapper dependency 추가

--- src/main/java/choi/oop/configuration/ApplicationConfiguration.java
+++ src/main/java/choi/oop/configuration/ApplicationConfiguration.java
@@ -1,8 +1,13 @@
 package choi.oop.configuration;
 
 import choi.oop.util.SystemEnvUtil;
+import org.modelmapper.*;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class ApplicationConfiguration {
@@ -10,6 +15,32 @@ public class ApplicationConfiguration {
     @Bean
     public SystemEnvUtil systemEnvUtil() {
         return new SystemEnvUtil();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        Provider<LocalDateTime> localDateTimeProvider = new AbstractProvider<LocalDateTime>() {
+            @Override
+            public LocalDateTime get() {
+                return LocalDateTime.now();
+            }
+        };
+
+        Converter<String, LocalDateTime> toStringDate = new AbstractConverter<String, LocalDateTime>() {
+            @Override
+            protected LocalDateTime convert(String source) {
+                DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+                return LocalDateTime.parse(source, format);
+            }
+        };
+
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.createTypeMap(String.class, LocalDateTime.class);
+        modelMapper.addConverter(toStringDate);
+        modelMapper.getTypeMap(String.class, LocalDateTime.class).setProvider(localDateTimeProvider);
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT);
+        return modelMapper;
     }
 
 }
