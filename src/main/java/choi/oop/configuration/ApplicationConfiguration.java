d1eed71984d17eb72a635b52145557ad5a1c096d
youngrackchoi <yacksock3@naver.com>
2025-02-04 16:42:18 +0900

    ModelMapper 정리 dependency 제거.

--- src/main/java/choi/oop/configuration/ApplicationConfiguration.java
+++ src/main/java/choi/oop/configuration/ApplicationConfiguration.java
@@ -1,39 +1,8 @@
 package choi.oop.configuration;
 
-import org.modelmapper.*;
-import org.modelmapper.convention.MatchingStrategies;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 @Configuration
 public class ApplicationConfiguration {
 
-    @Bean
-    public ModelMapper modelMapper() {
-        Provider<LocalDateTime> localDateTimeProvider = new AbstractProvider<LocalDateTime>() {
-            @Override
-            public LocalDateTime get() {
-                return LocalDateTime.now();
-            }
-        };
-
-        Converter<String, LocalDateTime> toStringDate = new AbstractConverter<String, LocalDateTime>() {
-            @Override
-            protected LocalDateTime convert(String source) {
-                DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-                return LocalDateTime.parse(source, format);
-            }
-        };
-
-        ModelMapper modelMapper = new ModelMapper();
-        modelMapper.createTypeMap(String.class, LocalDateTime.class);
-        modelMapper.addConverter(toStringDate);
-        modelMapper.getTypeMap(String.class, LocalDateTime.class).setProvider(localDateTimeProvider);
-        modelMapper.getConfiguration()
-                .setMatchingStrategy(MatchingStrategies.STRICT);
-        return modelMapper;
-    }
 }
